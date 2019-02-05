310d861457d8c45d4ad5dc4beb4ef8e34a8bb0e9
Bradley Francis <fradleybrancis@gmail.com>
2019-02-04 22:07:08 -0800

    "stringifyJSON working with Arrays"

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,20 +4,42 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
+  
   var newString = "";
-  var stringRay = '[]';
+  var stringRay = [];
+
+
   if (typeof(obj) === 'string') {
     return '"' + obj + '"';
   } else if (obj === null) {
     return 'null';
-  } else if (obj.length === 0) {
-    return stringRay;
-  } else if (obj.length === 1) {
-    return '[' + stringifyJSON(obj[0]) + ']';
-  } else if (obj.length > 2) {
-
+  } else if (typeof obj === 'number') {
+    return String(obj);
+  } else if (obj == {}) {
+    return '{}';
+  } else if (obj.length !== undefined && typeof obj === 'object') {
+    if (obj.length === 0) {
+      return '[]';
+    } else if (obj.length > 0) {
+        for (var i = 0; i < obj.length; i++) {
+          stringRay.push(stringifyJSON(obj[i]));
+        }
+        return '[' + stringRay + ']';        
+    }
+  } else if (obj.length === undefined && typeof obj === 'object' ) {
+    var keyCount = 0;    
+    for (var i in obj) {
+      keyCount++
+    }
+    if (keyCount === 0) {
+      return '{}';
+    }
   }
   
   return String(obj);
+
+
   
 };
+
+JSON.stringify(stringifiableObjects[7])
