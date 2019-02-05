c5ce439ca20ee6087ab3debe95100717661f0b93
Bradley Francis <fradleybrancis@gmail.com>
2019-02-04 23:41:32 -0800

    "stringifyJSON up and running!!!"

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,8 +5,9 @@
 
 var stringifyJSON = function(obj) {
   
-  var newString = "";
+  var str = "";
   var stringRay = [];
+  var strObject = {};
 
 
   if (typeof(obj) === 'string') {
@@ -23,23 +24,25 @@ var stringifyJSON = function(obj) {
     } else if (obj.length > 0) {
         for (var i = 0; i < obj.length; i++) {
           stringRay.push(stringifyJSON(obj[i]));
-        }
+        };
         return '[' + stringRay + ']';        
-    }
+    };
   } else if (obj.length === undefined && typeof obj === 'object' ) {
     var keyCount = 0;    
     for (var i in obj) {
-      keyCount++
-    }
+      keyCount++;
+      if (i === undefined || typeof i === 'function' || obj[i] === undefined || typeof obj[i] === 'function') {
+        str += '';
+      } else {
+        str += stringifyJSON(i) + ':' + stringifyJSON(obj[i]) + ',';
+      };
+    };
     if (keyCount === 0) {
       return '{}';
-    }
-  }
-  
-  return String(obj);
-
-
-  
+    };
+    
+    return '{' + str.slice(0, -1) + '}';
+ 
+  };  
+  return String(obj);  
 };
-
-JSON.stringify(stringifiableObjects[7])
