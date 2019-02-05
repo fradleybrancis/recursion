030c31722b13f758352989aa3910f1190ecdc2f0
Bradley Francis <fradleybrancis@gmail.com>
2019-02-05 15:43:32 -0800

    "getClass working but not passing tests"

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,42 @@
 // };
 
 // But instead we're going to implement it from scratch:
+// Use document.body, element.childNodes, and element.classList
 var getElementsByClassName = function(className
 ) {
-  
+
+  let classy = [];
+
+  var checkNodes = function(nodes) {
+
+    var kids = nodes.children;
+    
+    forEach(kids, function(element){
+      
+      if (element.classList === true && element.classList.contains(className)) {
+        classy.push(element);
+      };
+      
+      if (element.children) {
+        checkNodes(element.children);
+      };
+    });
+  };
+
+  checkNodes(document.body);
+
+  return classy;
+
+};
+
+var forEach = function(collection, iterator) {
+  if (Array.isArray(collection)) {
+    for(var i = 0; i < collection.length; i++) {
+      iterator(collection[i], i, collection);
+    };
+  } else {
+    for(var i in collection) {
+      iterator(collection[i], i, collection);
+    };
+  };
 };
